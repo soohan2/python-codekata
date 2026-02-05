d92734e8fcd67a237cc80764b7142bb187d74b08
soohan2 <105334509+soohan2@users.noreply.github.com>
2026-02-05 21:20:01 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,16 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 우수한
+# 작성일: 2026. 02. 05. 21:20:00
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = (n + 6) / 7;
+    return answer;
+}
+
